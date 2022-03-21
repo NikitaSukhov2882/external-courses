8fc3d5e040cb0a839edccf143959fa04c3b98954
NikitaSukhovEpam <100701982+NikitaSukhovEpam@users.noreply.github.com>
2022-03-21 18:38:56 +0300

    Удалил лишнюю переменную

--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -1,9 +1,7 @@
 const getUpperWord = (str) => {
     let array = str.split(' ');
-    let element;
     for (let index = 0; index < array.length; index++) {
-        element = array[index][0].toUpperCase();
-        array[index] = element + array[index].substr(1);
+        array[index] = array[index][0].toUpperCase() + array[index].substr(1);
     }
     
     return str = array.join(' ');;
--- src/ex4_js-objects-part2/task-08.js
+++ src/ex4_js-objects-part2/task-08.js
@@ -1,11 +1,8 @@
 const getCamelCase = (str) => {
     str = str.toLowerCase();
     let strElements = str.split (' ');
-    let element;
     for (let index = 1; index < strElements.length; index++) {
-        element = strElements[index][0].toUpperCase();
-        strElements[index] = element + strElements[index].substr(1);
-        
+        strElements[index] = strElements[index][0].toUpperCase() + strElements[index].substr(1); 
     }
     
     return str = strElements.join('');
