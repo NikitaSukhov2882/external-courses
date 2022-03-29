147755b03f5a029e6221a55ccbfa3127ac475cb1
NikitaSukhovEpam <100701982+NikitaSukhovEpam@users.noreply.github.com>
2022-03-29 18:13:59 +0300

    Поменял тернарные операторы и убрал index=newBegin

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -2,10 +2,10 @@ const sliceArray = (array, begin = 0, end = array.length) => {
     let newArray = [];
     let newBegin,
         newEnd;
-    begin >= 0 ? newBegin = begin : newBegin = array.length + begin;
-    end >= 0 ? newEnd = end : newEnd = array.length + end;
-    for (let index = newBegin, i = 0; index < newEnd; index++, i++) {
-        newArray[i] = array[index];
+    newBegin = begin >= 0 ? begin : array.length + begin;
+    newEnd = end >= 0 ? end : array.length + end;
+    for (let i = 0; newBegin < newEnd; newBegin++, i++) {
+        newArray[i] = array[newBegin];
     }
     
     return newArray;
